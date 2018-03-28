3d1a0a0ec697ab15346a1013782ee14f63fb12fa
patrykbaklaj@gmail.com <patrykbaklaj@gmail.com>
2018-03-28 21:14:34 +0100

    Change menu style after scroll

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,14 @@ import Popper from "popper.js/dist/popper-utils.js";
 
 $(document).ready(function () {
 
-
+    $(window).on('scroll', () => {
+        if($(window).scrollTop() >= 100) {
+            $('#nav').addClass('nav-white');
+            $('#nav').addClass('box-shadow');
+        } else {
+            $('#nav').removeClass('nav-white');
+            $('#nav').removeClass('box-shadow');
+        };
+    })
 
 });
